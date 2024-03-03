da206a3de960dfbc4baeefeb3c100bd56be3583e
New <jalilovjahongir99@gmail.com>
2024-03-03 13:49:04 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# def calculator():
+#     number = int(input('Birinchi raqamni kiriting: '))
+#     operator = input("Jarayonni tanlang (+, -, *, /): ")
+#     number1 = int(input('Birinchi raqamni kiriting: '))
+#
+#     if operator == '+':
+#         natija = number + number1
+#     elif operator == '-':
+#         natija = number - number1
+#     elif operator == '*':
+#         natija = number * number1
+#     elif operator == '/':
+#         natija = number / number1
+#     else:
+#         print("Noto'g'ri jarayon tanlandi.")
+#         return calculator()
+#
+#     print("Natija:", natija)
+#
+#
+# calculator()
+
+
